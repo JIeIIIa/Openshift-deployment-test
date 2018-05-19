04a6461fcd19d494167287fdf4e6e0d31993555f
JIeIIIa <stealth@bigmir.net>
2018-05-19 14:41:54 +0300

    Add debug information

--- src/main/java/com/google/javagapps/openshift/phonebook/OpenShiftDeployTestApplication.java
+++ src/main/java/com/google/javagapps/openshift/phonebook/OpenShiftDeployTestApplication.java
@@ -1,10 +1,35 @@
 package com.google.javagapps.openshift.phonebook;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenShiftDeployTestApplication {
+    @Value("${MYSQL_SERVICE_HOST}")
+    private String host;
+
+    @Value("${MYSQL_SERVICE_PORT}")
+    private String port;
+
+    @Value("${OPENSHIFT_MYSQL_USERNAME}")
+    private String user;
+
+    @Value("${OPENSHIFT_MYSQL_PASSWORD}")
+    private String password;
+
+    @Bean
+    public Object testInfo() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("MYSQL_SERVICE_HOST = " + host);
+        System.out.println("MYSQL_SERVICE_PORT = " + port);
+        System.out.println("MYSQL_SERVICE_USERNAME = " + user);
+        System.out.println("MYSQL_SERVICE_PASSWORD = " + password);
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        return new Object();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OpenShiftDeployTestApplication.class, args);
