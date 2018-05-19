4a07b190fff3f7206e5241915441c805499e173a
JIeIIIa <stealth@bigmir.net>
2018-05-19 18:29:01 +0300

    jar -> war

---
+++ src/main/java/com/google/javagapps/openshift/phonebook/Configuration/WebMvcConfiguration.java
@@ -0,0 +1,10 @@
+package com.google.javagapps.openshift.phonebook.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+}
